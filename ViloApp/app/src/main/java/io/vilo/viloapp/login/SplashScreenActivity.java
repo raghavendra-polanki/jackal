8fade2aca9bb0a0ef8d76f68996502ae721533a3
raghavendra-polanki <raghavendra@RaghavMac.local>
2017-08-05 17:45:50 +0530

    Changed splash screen as per latest deisgn

--- ViloApp/app/src/main/java/io/vilo/viloapp/login/SplashScreenActivity.java
+++ ViloApp/app/src/main/java/io/vilo/viloapp/login/SplashScreenActivity.java
@@ -1,6 +1,7 @@
 package io.vilo.viloapp.login;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -15,11 +16,14 @@ import butterknife.ButterKnife;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1500;
+    private static int SPLASH_TIME_OUT = 500;
 
 
-    @Bind(R.id.splash_image)
-    ImageView mSplashScreen;
+    @Bind(R.id.splash_screen_logo)
+    ImageView mSplashScreenLogo;
+
+    @Bind(R.id.splash_screen_name)
+    ImageView mSplashScreenName;
 
     private Intent mIntent;
 
@@ -28,6 +32,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         ButterKnife.bind(this);
+
 
     }
 
@@ -39,8 +44,25 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                goForward();
-//                finish();
+                mSplashScreenLogo.setColorFilter(Color.rgb(56,146,181));
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSplashScreenLogo.setColorFilter(Color.rgb(230,79,244));
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mSplashScreenLogo.setColorFilter(Color.rgb(4,251,27));
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        goForward();
+                                    }
+                                }, SPLASH_TIME_OUT);
+                            }
+                        }, SPLASH_TIME_OUT);
+                    }
+                }, SPLASH_TIME_OUT);
             }
         }, SPLASH_TIME_OUT);
 
@@ -55,17 +77,17 @@ public class SplashScreenActivity extends AppCompatActivity {
 //            content = bundle.getParcelable(KEY_PUSH_CONTENT);
 //        }
 
-        if(LocalStorage.getInstance().getUser() != null) {
+        //if(LocalStorage.getInstance().getUser() != null) {
             Intent intent = new Intent(SplashScreenActivity.this, HomeSliderActivity.class);
 //            if (content != null) {
 //                intent.putExtra(KEY_PUSH_CONTENT, content);
 //            }
             startActivity(intent);
             finish();
-        } else {
-            startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
-            finish();
-        }
+//        } else {
+//            startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+//            finish();
+//        }
 
     }
 
